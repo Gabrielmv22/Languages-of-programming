d193fee537708ad9d94a0b0bdf904c184c5ac78f
Gabrielmv22 <gabrielmino@uees.edu.ec>
2024-09-05 07:57:20 -0500

    ejercicios en clase

---
+++ Interpretado.py
@@ -0,0 +1,4 @@
+a=10
+b=15
+c=a+b
+print(f"El total de la suma es {c}")
---
+++ compilado.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int a=10;
+    int b=15;
+    int c=a+b;
+    printf("el total de la suma es %d",c);
+    return 0;
+}
